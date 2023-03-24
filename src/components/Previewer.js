d29fdbbbd71a57b5f35f03d7dc689b8c25346d88
utku-guclu <stanger307@live.com>
2023-03-24 14:46:19 -0700

    editor&previewer completed

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -4,6 +4,13 @@ import Toolbar from "../utils/Toolbar";
 import DOMPurify from "dompurify";
 import { marked } from "marked";
 
+import hljs from "highlight.js";
+
+marked.setOptions({
+  breaks: true,
+  highlight: (code) => hljs.highlightAuto(code).value,
+});
+
 function Previewer({ maxed, hidden, onMaxedToggle, text }) {
   return (
     <Window windowClass="previewer" {...{ maxed, hidden }}>
