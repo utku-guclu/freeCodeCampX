91a54e170b9ab3e5e0f0a9ee6b096751e9dda375
utku-guclu <stanger307@live.com>
2023-03-26 12:57:44 -0700

    sass && sound package installed && basic-layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import DrumMachine from "./components/DrumMachine";
+
 import React from "react";
 
 import "./App.css";
 
 function App() {
-  return <div id="app">hi</div>;
+  return <div id="app"><DrumMachine/></div>;
 }
 
 export default App;
