11bf042c6ec2f84c990da801a9b5d2b0b22102da
utku-guclu <stanger307@live.com>
2023-04-03 11:48:29 -0700

    simpple calculator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Calculator from "./components/Calculator";
+
 import React from "react";
 
 import "./App.css";
 
 function App() {
-  return <div id="app"></div>;
+  return (
+    <div id="app">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
