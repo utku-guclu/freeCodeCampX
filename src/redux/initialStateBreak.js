ebc652bde62b621b4db94dfe6d2ee90dd1b5a9be
Utku <34550887+utku-guclu@users.noreply.github.com>
2023-06-22 17:44:19 +0300

    Delete src directory

--- src/redux/initialStateBreak.js
+++
@@ -1,5 +0,0 @@
-const DEFAULT_BREAK_LENGTH = 5;
-
-export const initialState = {
-  count: DEFAULT_BREAK_LENGTH,
-};
--- src/redux/initialStateSession.js
+++
@@ -1,5 +0,0 @@
-const DEFAULT_SESSION_LENGTH = 25;
-
-export const initialState = {
-  count: DEFAULT_SESSION_LENGTH,
-};
