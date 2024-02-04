daf46753c1e6e55d5a0041ace7f3131258942ac8
utku-guclu <utku_guclu@hotmail.com>
2024-02-04 20:30:44 +0300

    security ch1

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,110 @@
-'use strict';
+"use strict";
+
+const StockModel = require("../models").Stock;
+
+const fetch = require("node-fetch");
+
+async function findStock(stock) {
+  return await StockModel.findOne({ symbol: stock }).exec();
+}
+
+async function createStock(stock, like, ip) {
+  const newStock = new StockModel({
+    symbol: stock,
+    likes: like ? [ip] : [],
+  });
+  const savedNew = await newStock.save();
+  return savedNew;
+}
+
+async function saveStock(stock, like, ip) {
+  let saved = {};
+  const foundStock = await findStock(stock);
+
+  if (!foundStock) {
+    const createSaved = await createStock(stock, like, ip);
+    saved = createSaved;
+    return saved;
+  } else {
+    if (like && foundStock.likes.indexOf(ip) === -1) {
+      foundStock.likes.push(ip);
+    }
+    saved = await foundStock.save();
+    return saved;
+  }
+}
+
+async function getStock(stock) {
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+  );
+  const { symbol, latestPrice } = await response.json();
+  return { symbol, latestPrice };
+}
 
 module.exports = function (app) {
+  app.route("/api/stock-prices").get(async function (req, res) {
+    const { stock, like } = req.query;
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    if (Array.isArray(stock)) {
+      console.log("stocks", stock);
+
+      const { symbol, latestPrice } = await getStock(stock[0]);
+      const { symbol: symbol2, latestPrice: latestPrice2 } = await getStock(
+        stock[1]
+      );
+
+      const firstStock = await saveStock(stock[0], like, req.ip);
+      const secondStock = await saveStock(stock[1], like, req.ip);
+
+      let stockData = [];
+
+      if (!symbol) {
+        stockData.push({
+          rel_likes: firstStock.likes.length * secondStock.likes.length,
+        });
+      } else {
+        stockData.push({
+          stock: symbol,
+          price: latestPrice,
+          rel_likes: firstStock.likes.length - secondStock.likes.length,
+        });
+      }
+
+      if (!symbol2) {
+        stockData.push({
+          rel_likes: secondStock.lieks.length - firstStock.likes.length,
+        });
+      } else {
+        stockData.push({
+          stock: symbol2,
+          price: latestPrice2,
+          rel_likes: secondStock.likes.length - firstStock.likes.length,
+        });
+      }
+      res.json({
+        stockData,
+      });
+
+      return;
+    }
+
+    const { symbol, latestPrice } = await getStock(stock);
+
+    if (!symbol) {
+      res.json({ stockData: { likes: like ? 1 : 0 } });
+      return;
+    }
+
+    const oneStockData = await saveStock(symbol, like, req.ip);
+    console.log("One Stock Data", oneStockData);
+
+    res.json({
+      stockData: {
+        stock: symbol,
+        price: latestPrice,
+        likes: oneStockData.likes.length,
+      },
     });
-    
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,111 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  test("Viewing one stock", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "AAPL" })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, "stockData");
+        assert.property(res.body.stockData, "stock");
+        assert.property(res.body.stockData, "price");
+        assert.property(res.body.stockData, "likes");
+        done();
+      });
+  });
 
+  test("Viewing one stock and liking it", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "AAPL", like: true })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, "stockData");
+        assert.property(res.body.stockData, "stock");
+        assert.property(res.body.stockData, "price");
+        assert.property(res.body.stockData, "likes");
+        assert.isAtLeast(res.body.stockData.likes, 1);
+        done();
+      });
+  });
+
+  test("Viewing the same stock and liking it again", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "AAPL", like: true })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, "stockData");
+        assert.property(res.body.stockData, "stock");
+        assert.property(res.body.stockData, "price");
+        assert.property(res.body.stockData, "likes");
+        const initialLikes = res.body.stockData.likes;
+
+        // Now, make a new request for the same stock and liking it again
+        chai
+          .request(server)
+          .get("/api/stock-prices")
+          .query({ stock: "AAPL", like: true })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.property(res.body, "stockData");
+            assert.property(res.body.stockData, "stock");
+            assert.property(res.body.stockData, "price");
+            assert.property(res.body.stockData, "likes");
+            assert.isAbove(res.body.stockData.likes, initialLikes);
+            done();
+          });
+      });
+  });
+
+  test("Viewing two stocks", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: ["AAPL", "GOOGL"] })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, "stockData");
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
+        assert.property(res.body.stockData[0], "stock");
+        assert.property(res.body.stockData[0], "price");
+        assert.property(res.body.stockData[0], "rel_likes");
+        assert.property(res.body.stockData[1], "stock");
+        assert.property(res.body.stockData[1], "price");
+        assert.property(res.body.stockData[1], "rel_likes");
+        done();
+      });
+  });
+
+  test("Viewing two stocks and liking them", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: ["AAPL", "GOOGL"], like: true })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, "stockData");
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
+        assert.property(res.body.stockData[0], "stock");
+        assert.property(res.body.stockData[0], "price");
+        assert.property(res.body.stockData[0], "rel_likes");
+        assert.property(res.body.stockData[1], "stock");
+        assert.property(res.body.stockData[1], "price");
+        assert.property(res.body.stockData[1], "rel_likes");
+        assert.isAbove(res.body.stockData[0].rel_likes, 0);
+        assert.isAbove(res.body.stockData[1].rel_likes, 0);
+        done();
+      });
+  });
 });
